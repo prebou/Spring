18255ac132b1bfbfcd4baa78913fb4ec3c57535c
prebou <prebou1999@gmail.com>
2024-11-22 12:36:14 +0100

    tp 1

---
+++ src/main/java/com/premiere/demo/hello/controleurs/HelloControleur.java
@@ -0,0 +1,15 @@
+package com.premiere.demo.hello.controleurs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloControleur {
+
+    @GetMapping
+    public String direHello(){
+        return "Hello";
+    }
+}
