161ee0920a2b3f0b6e31a453d1273800f5689a7b
prebou <prebou1999@gmail.com>
2024-11-25 17:29:19 +0100

    tp 5 fini

--- src/main/java/com/premiere/demo/entites/Ville.java
+++ src/main/java/com/premiere/demo/entites/Ville.java
@@ -1,17 +1,33 @@
 package com.premiere.demo.entites;
 
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
 public class Ville {
 
+    private static int Compteur_Id = 1;
+    private int id;
+
+    @NotNull
+    @Size(min = 2)
     private String nom;
+
+    @Min(1)
     private double nbHabitants;
 
     public Ville() {
     }
 
     public Ville(String nom, double nbHabitants) {
+        this.id = Compteur_Id++ ;
         this.nom = nom;
         this.nbHabitants = nbHabitants;
     }
+
+
 
     /**
      * Getter
@@ -47,5 +63,18 @@ public class Ville {
      */
     public void setNbHabitants(double nbHabitants) {
         this.nbHabitants = nbHabitants;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Setter
+     *
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
     }
 }
